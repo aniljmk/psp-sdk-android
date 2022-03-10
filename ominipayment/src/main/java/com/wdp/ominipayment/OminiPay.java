569a0e4941e0bd1ae082e0161d3f300c6f6ce000
narendra <narendra@webdevelopmentpark.com>
2022-03-10 14:37:45 +0530

    latest update 10 march 02:37 thu

---
+++ ominipayment/src/main/java/com/wdp/ominipayment/OminiPay.java
@@ -0,0 +1,41 @@
+package com.wdp.ominipayment;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+public class OminiPay {
+
+    public static void PaymentRequest(Context context, String username, String password, String key, String details){
+
+        Intent intent = new Intent(context,Payment.class);
+        intent.putExtra("username",username);
+        intent.putExtra("password",password);
+        intent.putExtra("key",key);
+        intent.putExtra("details",details);
+        ((Activity)context).startActivityForResult(intent,500);
+
+    }
+
+    public static void Subscription(Context context, String username, String password, String key, String details)
+    {
+        Intent intent = new Intent(context,Subscription.class);
+        intent.putExtra("username",username);
+        intent.putExtra("password",password);
+        intent.putExtra("key",key);
+        intent.putExtra("details",details);
+        ((Activity)context).startActivityForResult(intent,600);
+    }
+
+    public static void CancelSubscription(Context context, String username, String password, String key, String details)
+    {
+        Intent intent = new Intent(context,CancelSubscription.class);
+        intent.putExtra("username",username);
+        intent.putExtra("password",password);
+        intent.putExtra("key",key);
+        intent.putExtra("details",details);
+        ((Activity)context).startActivityForResult(intent,700);
+    }
+
+
+}
